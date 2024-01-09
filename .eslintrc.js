a7ca911f36fd836809d77034cd58e6d7b97f9ebc
deftones565 <Fearfactory2006@hotmail.com>
2024-01-09 15:42:25 +0400

    fixing lint issues with cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     "node": true,
     "es6": true,
     "jest/globals": true,
+    "cypress/globals": true,
     "commonjs": true,
     "browser": true,
   },
@@ -18,7 +19,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [

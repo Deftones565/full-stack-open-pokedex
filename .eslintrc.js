3b292087a49d57cf55ecac948c6a13ea27683262
deftones565 <Fearfactory2006@hotmail.com>
2024-01-09 15:06:44 +0400

    fixing lint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,9 @@ module.exports = {
   "env": {
     "node": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "commonjs": true,
+    "browser": true,
   },
   "extends": [
     "eslint:recommended",

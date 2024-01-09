b8304b6ad16d9c3e9e5494f3f996cf352298f0a0
deftones565 <Fearfactory2006@hotmail.com>
2024-01-09 16:23:49 +0400

    fixing e2e testing

--- cypress/e2e/pokemon.cy.js
+++ cypress/e2e/pokemon.cy.js
@@ -1,6 +1,6 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('EXTERNAL_API')
+    cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokemon and Pokemon character names are trademarks of Nintendo')
   })
